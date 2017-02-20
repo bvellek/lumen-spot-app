575b2834c21b44924775e208ab095ae0a43de009
bvellek <bvellek@uw.edu>
2017-02-20 15:18:05 -0800

    base actions

---
+++ public/js/actions/index.js
@@ -0,0 +1,12 @@
+// Convert Location to Lat/Long
+export const FETCH_GEO_SUCCESS = 'FETCH_GEO_SUCCESS';
+export const fetchGeoSuccess = () => ({
+  type: CREATE_GAME
+});
+
+
+// Search Location
+export const SEARCH_LOCATION = 'SEARCH_LOCATION';
+export const searchLocation = () => ({
+  type: CREATE_GAME
+});
