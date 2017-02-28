691731458b6604abbba0c06f7152935aea9f5082
bvellek <bvellek@uw.edu>
2017-02-28 14:35:29 -0800

    optimized images

--- public/js/components/image-slider.js
+++ public/js/components/image-slider.js
@@ -6,11 +6,11 @@ export default function ImageSlider() {
     <section className="photo-slide-section">
       <div id="slider">
         <figure>
-          <img src="./img/img-slider/01-min-circle.jpg" alt="circle on far left" />
-          <img src="./img/img-slider/02-min-circle.jpg" alt="circle moved toward right" />
-          <img src="./img/img-slider/03-min-circle.jpg" alt="cirle centered" />
-          <img src="./img/img-slider/04-min-circle.jpg" alt="circle to right" />
-          <img src="./img/img-slider/01-min-circle.jpg" alt="circle on far left" />
+          <img src="./img/img-slider/01-circle.jpg" alt="circle on far left" />
+          <img src="./img/img-slider/02-circle.jpg" alt="circle moved toward right" />
+          <img src="./img/img-slider/03-circle.jpg" alt="cirle centered" />
+          <img src="./img/img-slider/04-circle.jpg" alt="circle to right" />
+          <img src="./img/img-slider/01-circle.jpg" alt="circle on far left" />
         </figure>
       </div>
     </section>
