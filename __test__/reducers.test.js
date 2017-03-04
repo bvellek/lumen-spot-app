498195b81d60c91cb0e483db17ed3a39dc072ce6
bvellek <bvellek@uw.edu>
2017-03-03 16:45:16 -0800

    add remainder of reducer tests

--- __test__/reducers.test.js
+++ __test__/reducers.test.js
@@ -100,4 +100,109 @@ describe('Location Reducer', () => {
     error: 'ERR'
     })).toEqual(stateAfter);
   });
+
+  it('should handle FETCH_SUN_TIMES_SUCCESS (add sunTimesResults)', () => {
+    const testSun = '{sunrise: 5:30 am, sunset: 6:00pm}';
+    const stateAfter = {
+      locationCoords: null,
+      displayResults: false,
+      warningState: false,
+      warningMessage: null,
+      sunTimesResults: testSun,
+      weatherResults: null,
+      inspirationResults: null,
+      loadingStatus: false,
+    };
+    expect(locationReducer(initialState, {
+    type: 'FETCH_SUN_TIMES_SUCCESS',
+    results: testSun
+    })).toEqual(stateAfter);
+  });
+
+  it('should handle FETCH_SUN_TIMES_ERROR (change warning message, change warning state)', () => {
+    const stateAfter = {
+      locationCoords: null,
+      displayResults: false,
+      warningState: true,
+      warningMessage: 'ERR',
+      sunTimesResults: null,
+      weatherResults: null,
+      inspirationResults: null,
+      loadingStatus: false,
+    };
+    expect(locationReducer(initialState, {
+    type: 'FETCH_SUN_TIMES_ERROR',
+    error: 'ERR'
+    })).toEqual(stateAfter);
+  });
+
+  it('should handle FETCH_WEATHER_SUCCESS (add weatherResults)', () => {
+    const testWeather = '{wind: gusty, temp: 65.8}';
+    const stateAfter = {
+      locationCoords: null,
+      displayResults: false,
+      warningState: false,
+      warningMessage: null,
+      sunTimesResults: null,
+      weatherResults: testWeather,
+      inspirationResults: null,
+      loadingStatus: false,
+    };
+    expect(locationReducer(initialState, {
+    type: 'FETCH_WEATHER_SUCCESS',
+    weather: testWeather
+    })).toEqual(stateAfter);
+  });
+
+  it('should handle FETCH_WEATHER_ERROR (change warning message, change warning state)', () => {
+    const stateAfter = {
+      locationCoords: null,
+      displayResults: false,
+      warningState: true,
+      warningMessage: 'ERR',
+      sunTimesResults: null,
+      weatherResults: null,
+      inspirationResults: null,
+      loadingStatus: false,
+    };
+    expect(locationReducer(initialState, {
+    type: 'FETCH_WEATHER_ERROR',
+    error: 'ERR'
+    })).toEqual(stateAfter);
+  });
+
+  it('should handle FETCH_INSPIRATION_SUCCESS (add inspirationResults)', () => {
+    const testInspiration = '{iso: 800, ss: 1/500}';
+    const stateAfter = {
+      locationCoords: null,
+      displayResults: false,
+      warningState: false,
+      warningMessage: null,
+      sunTimesResults: null,
+      weatherResults: null,
+      inspirationResults: testInspiration,
+      loadingStatus: false,
+    };
+    expect(locationReducer(initialState, {
+    type: 'FETCH_INSPIRATION_SUCCESS',
+    inspiration: testInspiration
+    })).toEqual(stateAfter);
+  });
+
+  it('should handle FETCH_INSPIRATION_ERROR (change warning message, change warning state)', () => {
+    const stateAfter = {
+      locationCoords: null,
+      displayResults: false,
+      warningState: true,
+      warningMessage: 'ERR',
+      sunTimesResults: null,
+      weatherResults: null,
+      inspirationResults: null,
+      loadingStatus: false,
+    };
+    expect(locationReducer(initialState, {
+    type: 'FETCH_INSPIRATION_ERROR',
+    error: 'ERR'
+    })).toEqual(stateAfter);
+  });
 });
