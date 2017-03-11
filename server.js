928c814603ea79ae9c833f25e8a0786e98cfc64e
bvellek <bvellek@uw.edu>
2017-03-11 12:52:43 -0800

    organized match into a modular function, added comments to server, changed template to use production bundle.min.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// vendor imports
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
@@ -5,95 +6,25 @@ import 'isomorphic-fetch';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
-// import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+// component imports
 import routes from './public/js/components/router';
 import NotFoundPage from './public/js/components/not-found-page';
-// import * as reducers from './public/js/reducers/index';
 
+// redux imports
 import * as actions from './public/js/actions/index';
 import configureStore from './public/js/store';
 
 const app = express();
 
+app.set('view engine', 'pug'); // tells the render to use Pug template
+app.use(express.static(path.join(__dirname, 'public'))); // serving static assets
+app.use(express.static(path.join(__dirname, 'assets'))); // serving static assets
+app.use(bodyParser.json()); // for parsing the body of post requests
 
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'assets')));
-app.use(bodyParser.json());
-
-
-app.get('*', (req, res) => {
-  if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
-    res.redirect(`https://lumen-spot.herokuapp.com${req.url}`);
-  }
-  const store = configureStore();
-  console.log(store);
-
-  if (req.query.lat && req.query.lng) {
-    const lat = req.query.lat;
-    const lng = req.query.lng;
-    if (!isNaN(lat) && !isNaN(lng)) {
-      console.log('testingggggggg', lat, lng);
-      const coords = `${lat},${lng}`;
-      store.dispatch(actions.fetchLocationCoordsSuccess(coords));
-      return Promise.all([
-        store.dispatch(actions.fetchSunTimes(coords)), store.dispatch(actions.fetchWeather(coords)),
-        store.dispatch(actions.fetchInspiration(coords))
-      ]).then(() => {
-        match(
-          { routes, location: req.url || '/' },
-          (err, redirectLocation, renderProps) => {
-            if (err) {
-              return res.status(500).send(err.message);
-            }
-
-            if (redirectLocation) {
-              return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-            }
-
-            let markup;
-            let preloadedState;
-            if (renderProps) {
-              // const initialState = {
-              //   locationCoords: null,
-              //   displayResults: false,
-              //   warningState: false,
-              //   warningMessage: null,
-              //   sunTimesResults: null,
-              //   weatherResults: null,
-              //   inspirationResults: null,
-              //   coordsLoadingStatus: false,
-              //   routing: { location: renderProps.location }
-              // };
-
-              // const store = createStore(reducers.locationReducer, initialState);
-              preloadedState = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
-
-              const component = (
-                <Provider store={store}>
-                  <RouterContext {...renderProps} />
-                </Provider>
-              );
-
-              // if current route matched we have renderProps
-              console.log('*');
-              markup = renderToString(component);
-              console.log('**');
-            } else {
-              // otherwise render 404 page
-              markup = renderToString(<NotFoundPage />);
-              res.status(404);
-            }
-            console.log(preloadedState, '***************', preloadedState); // JSON.stringify(preloadedState).replace(/</g, '\\u003c')
-            return res.render('index', { react: markup, state: preloadedState });
-          }
-        );
-      });
-    }
-  }
-
+// matches the url and directs to correct response
+function matcher(req, res, store) {
   match(
     { routes, location: req.url || '/' },
     (err, redirectLocation, renderProps) => {
@@ -106,20 +37,12 @@ app.get('*', (req, res) => {
       }
 
       let markup;
-      if (renderProps) {
-        // const initialState = {
-        //   locationCoords: null,
-        //   displayResults: false,
-        //   warningState: false,
-        //   warningMessage: null,
-        //   sunTimesResults: null,
-        //   weatherResults: null,
-        //   inspirationResults: null,
-        //   coordsLoadingStatus: false,
-        //   routing: { location: renderProps.location }
-        // };
+      let preloadedState;
 
-        // const store = createStore(reducers.locationReducer, initialState);
+
+      if (renderProps) { // if current route matched we have renderProps
+        // the state we will provide for initial client-side (so the server and client match)
+        preloadedState = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
 
         const component = (
           <Provider store={store}>
@@ -127,17 +50,48 @@ app.get('*', (req, res) => {
           </Provider>
         );
 
-        // if current route matched we have renderProps
+        // this is where the components get rendered to a string to be inserted in Pug template
         markup = renderToString(component);
-      } else {
-        // otherwise render 404 page
+      } else { // otherwise render 404 page
         markup = renderToString(<NotFoundPage />);
         res.status(404);
       }
-      return res.render('index', { react: markup });
+      // rendering the markup and preloadedState into the pug template
+      return res.render('index', { react: markup, state: preloadedState });
     }
   );
+}
+
+app.get('*', (req, res) => {
+  // force use of https://
+  if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+    res.redirect(`https://lumen-spot.herokuapp.com${req.url}`);
+  }
+
+  // initialize store
+  const store = configureStore();
+
+  // check to see if a lat and lng are provided in the query string of URL
+  if (req.query.lat && req.query.lng) {
+    const lat = req.query.lat;
+    const lng = req.query.lng;
+
+    // check to see that the lat and lng are numbers - and fetch & render appropriate content for lat/lng
+    if (!isNaN(lat) && !isNaN(lng)) {
+      const coords = `${lat},${lng}`;
+      store.dispatch(actions.fetchLocationCoordsSuccess(coords));
+      return Promise.all([
+        store.dispatch(actions.fetchSunTimes(coords)), store.dispatch(actions.fetchWeather(coords)),
+        store.dispatch(actions.fetchInspiration(coords))
+      ]).then(() => {
+        matcher(req, res, store);
+      });
+    }
+  }
+  // if no lat/lng in query string of URL then just load/render inital state
+  matcher(req, res, store);
 });
+
 
 // Sun Times API proxy
 app.post('/location', (req, res) => {
@@ -155,6 +109,8 @@ app.post('/location', (req, res) => {
   });
 });
 
+
+// start server on https://localhost:8081/ or on the environmental PORT (for heroku)
 app.listen(process.env.PORT || 8081);
 console.log(`the server is listening on port ${process.env.PORT || 8081}`);
 console.log(`Visit localhost:${process.env.PORT || 8081}`);
